c32f4e430f29a29fe3740e529b7ad0d652e2bcee
kmicic91 <bilicz.adrian@gmail.com>
2017-06-14 14:15:06 -0400

    rendering countdown

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-let clock = null;
+const ONE_SECOND = 1000;
+class Clock {
+	constructor(){
+		this.currentTime = 0;
+		this.$clock = document.querySelector('h1');
+		this.clock = null;
+	}
+	render(string){
+		this.$clock.textContent = string;
+	}
+	start(formattedTime){
+		this.limitTime = Clock.parseSeconds(formattedTime)
+		
+		this.clock = setInterval( () => {
+			this.currentTime += ONE_SECOND
+			let diff = this.limitTime - this.currentTime;
+			this.render(diff)
+		}, ONE_SECOND)
+	}
+	static parseSeconds(time){
+		let [minutes,seconds] = time.split(':').map(Number)
+		return minutes * 60 * ONE_SECOND + seconds * ONE_SECOND;
+
+	}
+}
+
 function setup(){
-	$clock = document.querySelector('h1');
-	$clock.textContent = '00;00'
+	
+	let clock1 = new Clock();
+	clock1.start('10:00')
 }
 
 window.addEventListener('DOMContentLoaded', setup);
